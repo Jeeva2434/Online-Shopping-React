d3ecb26cb4f67058e8d404a7327fabc66362e985
jeeva <jeevanandam.s@heptagon.in>
2023-09-07 21:58:24 +0530

    partial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <BrowserRouter>
+      <main className='App'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
 }
 
 export default App;
