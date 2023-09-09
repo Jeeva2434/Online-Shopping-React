b993579d7a4ab379a3a116ed711d666c9e4e42df
jeevanandam <jeeva2434@gmail.com>
2023-09-09 22:40:55 +0530

    seafrch field hide in cart section condition

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,10 +2,14 @@ import React from 'react';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import {IoFilter} from 'react-icons/io5';
 import { GlobalState } from '../ContextAPI';
+import { useLocation } from 'react-router-dom';
 
 const Filter = () => {
 
   const{setFilterOpt} = GlobalState();
+
+  const locate = useLocation();
+  if(locate.pathname.split('/')[1] === 'cart') return;
 
   return (
     <DropdownButton id="filter" title={<IoFilter/>}>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Search from './Search';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const locate = useLocation();
+
   return (
     <header className='bg-dark text-light p-3 p-md-5 d-flex flex-wrap flex-md-nowrap justify-content-between'>
       <div className='col-12 col-md-3 logo fw-bold'>Smart Store</div>
-      <div className='col-12  col-md-3 text-start text-md-end my-4 my-md-0'>
-        <Search/>
-      </div>
+      {locate.pathname.split('/')[1] !== 'cart' &&
+        <div className='col-12  col-md-3 text-start text-md-end my-4 my-md-0'>
+          <Search/>
+        </div>
+      }
    </header>
   )
 }
